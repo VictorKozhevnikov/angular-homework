13c73a441108be5d767e94d0a067a9676ae09ab6
Victor Kozhevnikov <victor.kozhevnikov.consultant@consumer.org>
2017-03-15 11:08:47 +0300

    Extract search page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from  '@angular/core';
 
 @Component({
   selector: 'courses-header',
-  template: require('./header.component.template.html')
+  template: require('./header.component.html')
 })
 export class HeaderComponent {
 };
---
+++ src/app/pages/courses/search/search-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { SearchPageComponent } from './search-page.component';
+
+@NgModule({
+    declarations: [SearchPageComponent],
+    imports: [],
+    exports: [SearchPageComponent]
+})
+export class SearchPageModule {
+    constructor() { }
+}
