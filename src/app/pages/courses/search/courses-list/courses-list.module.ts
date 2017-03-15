c651a7a6740ea29429a023a753d3926a412ecbae
Victor Kozhevnikov <victor.kozhevnikov.consultant@consumer.org>
2017-03-15 11:25:41 +0300

    Extract list item component

--- src/app/pages/courses/search/courses-list/courses-list.module.ts
+++ src/app/pages/courses/search/courses-list/courses-list.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CoursesListComponent } from './courses-list.component';
+import { CourseListItemComponent } from './list-item/course-list-item.component';
 
 @NgModule({
-    declarations: [CoursesListComponent],
+    declarations: [CoursesListComponent, CourseListItemComponent],
     imports: [],
     exports: [CoursesListComponent]
 })
---
+++ src/app/pages/courses/search/courses-list/list-item/course-list-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from  '@angular/core';
+
+@Component({
+    selector: 'course-list-item',
+    template: require('./course-list-item.component.html')
+})
+export class CourseListItemComponent {
+}
