f2acab36dda4d9ea5f749bcdd165decff99b390d
Victor Kozhevnikov <victor.kozhevnikov.consultant@consumer.org>
2017-03-15 10:42:08 +0300

    extract header component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,8 @@
-import {Component} from  '@angular/core';
+import { Component } from  '@angular/core';
 
 @Component({
   selector: 'courses-footer',
   template: require ('./footer.component.template.html')
 })
-export class FooterComponent{
+export class FooterComponent {
 };
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from  '@angular/core';
+
+@Component({
+  selector: 'courses-header',
+  template: require('./header.component.template.html')
+})
+export class HeaderComponent {
+};
