19ee542b05c81986c48e57b9e902daabef382650
Victor Kozhevnikov <victor.kozhevnikov.consultant@consumer.org>
2017-03-22 18:41:11 +0300

    [()] binding and (click) for find button

--- src/app/pages/courses/search/toolbox/toolbox.component.ts
+++ src/app/pages/courses/search/toolbox/toolbox.component.ts
@@ -1,8 +1,13 @@
-import { Component } from  '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'toolbox',
     template: require('./toolbox.component.html')
 })
 export class ToolboxComponent {
+    private text: string;
+
+    private search(text: string): void {
+        console.log('---- search: ' + text + ', field value is ' + this.text);
+    }
 }
