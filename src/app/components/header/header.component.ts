fbd3b57efa4194188a92b4c11eb4ce580e7fec0f
Victor Kozhevnikov <victor.kozhevnikov.consultant@consumer.org>
2017-03-31 12:16:55 +0300

    bound user name

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,31 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, Inject } from '@angular/core';
+
+import { AuthService, authServiceToken } from '../../domain/auth';
+
 
 @Component({
     selector: 'courses-header',
     template: require('./header.component.html')
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    public isAnonymous: boolean;
+    public isAuthenticated: boolean;
+    public userName: string;
 
     @Output() public logoutRequested = new EventEmitter();
 
-    private logout() {
+    public constructor(
+        @Inject(authServiceToken) private readonly authService: AuthService
+    )
+    { }
+
+    public ngOnInit(): void {
+        this.isAuthenticated = this.authService.IsAuthenticated();
+        this.isAnonymous = !this.authService.IsAuthenticated();
+        this.userName = this.authService.GetUserInfo();
+    }
+
+    private logout(): void {
         this.logoutRequested.emit();
     }
 
--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 
 @NgModule({
-	declarations: [HeaderComponent],
-	imports: [],
-	exports: [HeaderComponent]
+    declarations: [HeaderComponent],
+    imports: [CommonModule],
+    exports: [HeaderComponent]
 })
 export class HeaderModule {
-	constructor() {
-	}
+    constructor() {
+    }
 }
